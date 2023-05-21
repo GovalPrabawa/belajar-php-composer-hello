063115ee19215154544f83c04acb12f676a031ff
GovalPrabawa <imadegoval726@gmail.com>
2023-05-21 18:22:24 +0800

    menambahkan file

--- coba.php
+++ coba.php
@@ -5,4 +5,5 @@ require_once __DIR__ . "/src/Customer.php";
 use Govalprogrammer\Belajar\Customer;
 
 $people = new Customer("Budi");
-echo $people->sayHello("Yadnya");
+echo $people->sayHello("Yadnya") . PHP_EOL;
+echo $people->sayHello();
--- src/Customer.php
+++ src/Customer.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Govalprogrammer\Belajar;
+namespace Govalprogrammer\BelajarPhpComposerHello;
 
 class Customer
 {
